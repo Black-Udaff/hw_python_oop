e1ab9c99ba8cf8f81dcb9e3bb134feada11ee3ee
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-07-11 01:02:08 +0300

    Для ноута

--- homework.py
+++ homework.py
@@ -1,21 +1,38 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
-    pass
+    def __init__(self, training_type, duration, distance, speed, calories):
+        self.trainigtype = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def get_message(self):
+        return (
+            f'Тип тренировки: {self.training_type};'
+            f'Длительность: {self.duration} ч.;'
+            f'Дистанция: {self.distance} км;'
+            f'Ср. скорость: {self.speed} км/ч; '
+            f'Потрачено ккал: {self.calories}.'
+        )
 
 
 class Training:
     """Базовый класс тренировки."""
 
-    LEN_STEP = 0.65  # длина шага в метрах
+    LEN_STEP = 0.65    # длина шага в метрах
     LEN_GREBOK = 1.38  # длина гребка в метрах
-    M_IN_KM = 1000  # константа для перевода значений из метров в километры
+    M_IN_KM = 1000     # константа для перевода значений из метров в километры(и иобратно)
+    MIN_IN_HOUR = 60   # кол-во минут в часе
+    SEC_IN_HOUR = 3600 # кол-во секунд в часе
+    SM_IN_M = 100      # колво сантимертов в метре
 
     def __init__(
         self,
-        action: int,  # колличество совершенных действий гребков или шагов
-        duration: float,  # время в часах
-        weight: float,  # вес в Кг?
+        action: int,     # колличество совершенных действий гребков или шагов
+        duration: float, # время в часах
+        weight: float,   # вес в Кг?
     ) -> None:
         self.action = action
         self.duration = duration
@@ -37,18 +54,43 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+        message = InfoMessage.get_message(
+            self.__class__.__name__,
+            self.duration,
+            self.distance,
+            self.speed,
+            self.calories,
+        )
+        return message
 
 
 class Running(Training):
     """Тренировка: бег."""
-
-    pass
+    CALORIES_MEAN_SPEED_MULT = 18
+    CALORIES_MEAN_SPEED_SHIFT = 1.79 
+    def get_spent_calories(self) -> float:
+        calories = ((self.CALORIES_MEAN_SPEED_MULT * self.get_mean_speed()*self.M_IN_KM  + self.CALORIES_MEAN_SPEED_SHIFT) 
+                    * self.weight / self.M_IN_KM * (self.duration * self.MIN_IN_HOUR) )
+        return calories
+    
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+    CALORIES_MEAN_WEIGTH_MULT = 0.035
+    def __init__(
+        self,
+        action: int,  # колличество совершенных действий гребков или шагов
+        duration: float,  # время в часах
+        weight: float,  # вес в Кг?
+        height: int # Рост в сантиметрах
+    ) -> None:
+        super().__init__(action, duration, weight)
+        self.height = height
 
+     def get_spent_calories(self) -> float:   
+        ((self.CALORIES_MEAN_WEIGTH_MULT * self.weight + ((self.get_mean_speed()*self.M_IN_KM/self.SEC_IN_HOUR)**2 / рост_в_метрах)
+            * 0.029 * вес) * время_тренировки_в_минутах)
     pass
 
 
